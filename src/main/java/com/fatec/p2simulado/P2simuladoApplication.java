4ec6e3d5e7e2e6f2e5d1d47f94647dbfb01b3f1d
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2023-06-13 08:19:20 -0300

    :tada: Initial commit

---
+++ src/main/java/com/fatec/p2simulado/P2simuladoApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.p2simulado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2simuladoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2simuladoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/p2simulado/P2simuladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.p2simulado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2simuladoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
